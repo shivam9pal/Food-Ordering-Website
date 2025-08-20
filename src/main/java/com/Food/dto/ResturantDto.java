4d02c23b03f0ab5823b9364ac3eb609c53d6207f
Angel Gruevski <angelgruevski@192.168.0.113>
2025-08-20 11:22:00 +0200

    Removed redundant dependency, removed @Id from embeddable which was causing the issue and added generation strategy in the Food class.

--- src/main/java/com/Food/dto/ResturantDto.java
+++ src/main/java/com/Food/dto/ResturantDto.java
@@ -1,21 +1,14 @@
 package com.Food.dto;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
 @Data
 @Embeddable// used when we want to insert into another entity
 public class ResturantDto {
-
-    @Id
-    private Long id;
     private String title;
     @Column(length=1000)
     private List<String> images;
